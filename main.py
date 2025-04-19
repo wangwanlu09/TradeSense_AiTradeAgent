5f1c279ea36c65ef3a2f76ab32043d635c266238
wang wanlu <146597120+wangwanlu09@users.noreply.github.com>
2025-04-19 16:20:45 +1200

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from routes import news, strategy, market
 
 app = FastAPI()
 
-# 根路由
+
 @app.get("/")
 def root():
     return {"message": "Welcome to the AI Trade Agent API"}
 
-# 引入并注册新闻路由
+
 app.include_router(news.router)
 app.include_router(strategy.router)
 app.include_router(market.router)
